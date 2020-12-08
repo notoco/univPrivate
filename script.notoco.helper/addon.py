d427360a611232d253b3c688db9f68bbb25ea1ac
notoco <36659110+notoco@users.noreply.github.com>
2020-12-08 18:17:31 +0100

    Up
    Hyperion switch
    notoco helper
    Poprawka Aeon - notyfikacja

---
+++ script.notoco.helper/addon.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import xbmc
+import control
+state = control.get_setting('state')
+if __name__ == '__main__':
+    arg = None
+
+    try:
+       arg = sys.argv[1].lower()
+    except Exception:
+       pass
+# AMBILIGHT
+    if arg == "amb_on":
+        control.turn_on()
+    elif arg == "amb_off":
+        control.turn_off()
+    elif arg == "amb_switch":
+        if state == 'true':
+            control.turn_off()
+        else:
+            control.turn_on()
+#ESC
+    elif arg == "esc":
+        xbmc.executebuiltin("Action(Stop)")
+        xbmc.executebuiltin("Dialog.Close(all, true)")
+        xbmc.executebuiltin("xbmc.ActivateWindow(home)")
+#EPG
+    elif arg == "epg":
+        playing = xbmc.Player().isPlayingVideo()
+        if (playing == True):  
+            xbmc.executebuiltin("xbmc.ActivateWindow(fullscreenvideo)")
+        else:
+            xbmc.executebuiltin("Action(back)")
--- script.notoco.helper/control.py
+++ script.notoco.helper/control.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 import xbmcaddon
 import xbmcgui
-import requests
+import os, sys
 
 addon = xbmcaddon.Addon()
-addonname = addon.getAddonInfo('name')
-state = server_enable = addon.getSetting('state')
+state = addon.getSetting('state')
 icon = addon.getAddonInfo('icon')
+amb_bright = addon.getSetting('bright')
 
 def turn_off():
     os.system("hyperion-remote -L 0")
@@ -15,11 +15,11 @@ def turn_off():
 
 def turn_on():
     os.system("hyperion-remote -L " + amb_bright)
-    addon.setSetting('state', 'false')
+    addon.setSetting('state', 'true')
     send_notification("Ambilight", "On")
 
-def send_notification(message):
-    xbmcgui.Dialog().notification(addonname, message, icon=icon)
+def send_notification(komponent, message):
+    xbmcgui.Dialog().notification(komponent, message, icon=icon)
 
 def get_setting(name):
     return addon.getSetting(name)
